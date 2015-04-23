bd22e46e7b2571c5503d39149433be4bfdc778d1
Gusshail <gusshail@student.gu.se>
2015-04-23 13:25:18 +0200

    added a constructor for questions and a db connector

--- app/src/main/java/mysecondapplication/android/com/mysecondapplication/DatabaseHandler.java
+++ app/src/main/java/mysecondapplication/android/com/mysecondapplication/DatabaseHandler.java
@@ -5,6 +5,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 
+import java.sql.SQLException;
+
+
 /**
  * Created by Aleksandar and Ilya on 4/1/2015.
  */
@@ -12,30 +15,49 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> c452e901468df05df37b4d4e266074e45b667a8a
+    private static final String DATABASE_NAME = "dummy_database.db",
 
-    private static final String DATABASE_NAME = "dummy_database",
     TABLE_QUESTIONS = "questions",
-    KEY_ID = "id",
-    KEY_QUESTIONS = "question",
-    KEY_ANSWER = "answer";
+            KEY_ID = "id",
+            KEY_QUESTIONS = "question",
+            KEY_ANSWER = "answer";
 
-    public DatabaseHandler (Context new_context) {
+    private static final String CREATE_TABLE = "NEW_TABLE" + TABLE_QUESTIONS + "(" + KEY_ID + "Number of question" + KEY_QUESTIONS  + "Question" + KEY_ANSWER + "Answer)";
 
-        super(new_context, DATABASE_NAME, null, DATABASE_VERSION);
+    public DatabaseHandler (Context context) {
+
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
 
     }
 
     @Override
     public void onCreate (SQLiteDatabase db) {
 
-        db.execSQL("NEW_TABLE" + TABLE_QUESTIONS + "(" + KEY_ID + "Number of question" + KEY_QUESTIONS + "Questions" + KEY_ANSWER + "Answer)");
+        db.execSQL(CREATE_TABLE);
+        addQuestions();
+
+
+
+
 
 
     }
+
+    private void addQuestions() {
+
+        Question id1  = new Question("Is St James capital of New Zealand", "yes", "no", "no");
+
+
+
+    }
+
+
+
+
+
+
+
     @Override
     public void  onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion ) {
         db.execSQL("Drop the table" + TABLE_QUESTIONS);
